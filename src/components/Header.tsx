f8e96718d1a4a6e97fe791b8aa147d10943e8174
Mikeal <mwiemszra@gmail.com>
2024-06-04 19:24:19 +0200

    🚧 Work on Interface layout components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,12 @@
+'use client';
 // Next
 import Image from "next/image";
 import Link from "next/link";
 // Components
 import { ModeToggle } from "./mode-toggle";
+import { MobileNavBar } from "./NavBar";
 // Icons
-import {
-  CircleUser,
-  Home,
-  LineChart,
-  Package,
-  Package2,
-  PanelLeft,
-  Search,
-  ShoppingCart,
-  Users2,
-} from "lucide-react"
+import { CircleUser, Search } from "lucide-react"
 // UI
 import {
   Breadcrumb,
@@ -35,65 +27,13 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { Input } from "@/components/ui/input"
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet" // ? What is this?
 
 export default function Header() {
   return (
     <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
-      <Sheet>
-        <SheetTrigger asChild>
-          <Button size="icon" variant="outline" className="sm:hidden">
-            <PanelLeft className="h-5 w-5" />
-            <span className="sr-only">Toggle Menu</span>
-          </Button>
-        </SheetTrigger>
-        <SheetContent side="left" className="sm:max-w-xs">
-          <nav className="grid gap-6 text-lg font-medium">
-            <Link
-              href="#"
-              className="group flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:text-base"
-            >
-              <Package2 className="h-5 w-5 transition-all group-hover:scale-110" />
-              <span className="sr-only">Acme Inc</span>
-            </Link>
-            <Link
-              href="#"
-              className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-            >
-              <Home className="h-5 w-5" />
-              Dashboard
-            </Link>
-            <Link
-              href="#"
-              className="flex items-center gap-4 px-2.5 text-foreground"
-            >
-              <ShoppingCart className="h-5 w-5" />
-              Orders
-            </Link>
-            <Link
-              href="#"
-              className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-            >
-              <Package className="h-5 w-5" />
-              Products
-            </Link>
-            <Link
-              href="#"
-              className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-            >
-              <Users2 className="h-5 w-5" />
-              Customers
-            </Link>
-            <Link
-              href="#"
-              className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-            >
-              <LineChart className="h-5 w-5" />
-              Settings
-            </Link>
-          </nav>
-        </SheetContent>
-      </Sheet>
+      {/* Mobile Sidebar */}
+      <MobileNavBar />
+      {/* Breadcrumb Path */}
       <Breadcrumb className="hidden md:flex">
         <BreadcrumbList>
           <BreadcrumbItem>
@@ -113,6 +53,7 @@ export default function Header() {
           </BreadcrumbItem>
         </BreadcrumbList>
       </Breadcrumb>
+      {/* SearchBar */}
       <div className="relative ml-auto flex-1 md:grow-0">
         <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
         <Input
@@ -121,7 +62,9 @@ export default function Header() {
           className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
         />
       </div>
+      {/* Mode Toggle */}
       <ModeToggle />
+      {/* Dropdown Menu */}
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button
@@ -129,7 +72,7 @@ export default function Header() {
             size="icon"
             className="overflow-hidden rounded-full"
           >
-            <CircleUser className="h-5 w-5" />
+            <CircleUser className="h-5 w-5" /> {/* // TODO: Handle unloged user and user without profile */}
             {/* <Image
               src="/placeholder-user.jpg"
               width={36}
@@ -139,15 +82,21 @@ export default function Header() {
             /> */}
           </Button>
         </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>Settings</DropdownMenuItem>
-          <DropdownMenuItem>Support</DropdownMenuItem>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>Logout</DropdownMenuItem>
-        </DropdownMenuContent>
+        <Menu />
       </DropdownMenu>
     </header>
+  );
+}
+
+function Menu() { // TODO: handle unloged user (display login and sign in, instead of profile menu)
+  return (
+    <DropdownMenuContent align="end"> {/* handle unloged user (login sign in) */}
+      <DropdownMenuLabel>My Account</DropdownMenuLabel>
+      <DropdownMenuSeparator />
+      <DropdownMenuItem>Settings</DropdownMenuItem>
+      <DropdownMenuItem>Support</DropdownMenuItem>
+      <DropdownMenuSeparator />
+      <DropdownMenuItem>Logout</DropdownMenuItem>
+    </DropdownMenuContent>
   );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,18 @@
+'use client';
 // Next
 import Link from "next/link";
+import { usePathname } from 'next/navigation'
 // Icons
 import {
   Home,
   LineChart,
   Package,
   Package2,
+  PanelLeft,
   Settings,
   ShoppingCart,
   Users2,
 } from "lucide-react"
-
 // UI
 import {
   Tooltip,
@@ -18,105 +20,132 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip"
+import { Button } from "@/components/ui/button"
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
 
+// Desktop NavBar
 export default function NavBar() {
   return (
     <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
       <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
         <Link
-          href="#"
-          className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
+          href="/"
+          className={"group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"}
         >
-          <Package2 className="h-4 w-4 transition-all group-hover:scale-110" />
-          <span className="sr-only">Acme Inc</span>
+          <Package2 className="h-4 w-4 transition-all group-hover:scale-110" /> {/* Or printspot logo */}
+          <span className="sr-only">PrintSpot</span>
         </Link>
-        <TooltipProvider>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Link
-                href="#"
-                className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-              >
-                <Home className="h-5 w-5" />
-                <span className="sr-only">Dashboard</span>
-              </Link>
-            </TooltipTrigger>
-            <TooltipContent side="right">Dashboard</TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
-        <TooltipProvider>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Link
-                href="#"
-                className="flex h-9 w-9 items-center justify-center rounded-lg bg-accent text-accent-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-              >
-                <ShoppingCart className="h-5 w-5" />
-                <span className="sr-only">Orders</span>
-              </Link>
-            </TooltipTrigger>
-            <TooltipContent side="right">Orders</TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
-        <TooltipProvider>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Link
-                href="#"
-                className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-              >
-                <Package className="h-5 w-5" />
-                <span className="sr-only">Products</span>
-              </Link>
-            </TooltipTrigger>
-            <TooltipContent side="right">Products</TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
-        <TooltipProvider>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Link
-                href="#"
-                className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-              >
-                <Users2 className="h-5 w-5" />
-                <span className="sr-only">Customers</span>
-              </Link>
-            </TooltipTrigger>
-            <TooltipContent side="right">Customers</TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
-        <TooltipProvider>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Link
-                href="#"
-                className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-              >
-                <LineChart className="h-5 w-5" />
-                <span className="sr-only">Analytics</span>
-              </Link>
-            </TooltipTrigger>
-            <TooltipContent side="right">Analytics</TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
+        <Option label="Dashboard" href="/dashboard">
+          <Home className="h-5 w-5" />
+        </Option>
+        <Option label="Orders" href="/orders">
+          <ShoppingCart className="h-5 w-5" />
+        </Option>
+        <Option label="Products" href="/products">
+          <Package className="h-5 w-5" />
+        </Option>
+        <Option label="Customers" href="/customers">
+          <Users2 className="h-5 w-5" />
+        </Option>
+        <Option label="Analytics" href="/analytics">
+          <LineChart className="h-5 w-5" />
+        </Option>
       </nav>
       <nav className="mt-auto flex flex-col items-center gap-4 px-2 sm:py-5">
-        <TooltipProvider>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Link
-                href="#"
-                className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-              >
-                <Settings className="h-5 w-5" />
-                <span className="sr-only">Settings</span>
-              </Link>
-            </TooltipTrigger>
-            <TooltipContent side="right">Settings</TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
+        <Option label="Settings" href="/settings">
+          <Settings className="h-5 w-5" />
+        </Option>
       </nav>
     </aside>
+  );
+}
+
+interface OptionProps {
+  children: React.ReactNode;
+  label: string;
+  href: string;
+}
+
+function Option({ children, label, href }: Readonly<OptionProps>) {
+  const pathname = usePathname().split("/")[1];
+  const hrefPath = href.split("/")[1];
+
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Link
+            href={href}
+            className={`flex h-9 w-9 items-center justify-center rounded-lg transition-colors hover:text-foreground md:h-8 md:w-8 ${pathname === hrefPath ? "bg-accent text-accent-foreground" : "text-muted-foreground"}`}
+          >
+            {children}
+            <span className="sr-only">{label}</span>
+          </Link>
+        </TooltipTrigger>
+        <TooltipContent side="right">{label}</TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  )
+}
+
+// Mobile NavBar
+export function MobileNavBar() {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button size="icon" variant="outline" className="sm:hidden">
+          <PanelLeft className="h-5 w-5" />
+          <span className="sr-only">Toggle Menu</span>
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="sm:max-w-xs">
+        <nav className="grid gap-6 text-lg font-medium">
+          <Link
+            href="#"
+            className="group flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:text-base"
+          >
+            <Package2 className="h-5 w-5 transition-all group-hover:scale-110" />
+            <span className="sr-only">PrintSpot</span>
+          </Link>
+          <MobileOption label="Dashboard" href="/dashboard">
+            <Home className="h-5 w-5" />
+          </MobileOption>
+          <MobileOption label="Orders" href="/orders">
+            <ShoppingCart className="h-5 w-5" />
+          </MobileOption>
+          <MobileOption label="products" href="/products">
+            <Package className="h-5 w-5" />
+          </MobileOption>
+          <MobileOption label="Customers" href="/customers">
+            <Users2 className="h-5 w-5" />
+          </MobileOption>
+          <MobileOption label="Settings" href="/settings">
+            <LineChart className="h-5 w-5" />
+            Settings
+          </MobileOption>
+        </nav>
+      </SheetContent>
+    </Sheet>
+  )
+}
+
+interface MobileOptionProps {
+  children: React.ReactNode;
+  label: string;
+  href: string;
+}
+
+function MobileOption({ children, label, href }: Readonly<MobileOptionProps>) { // TODO: Handling unloged user (maybe return null)
+  const pathname = usePathname().split("/")[1];
+  const hrefPath = href.split("/")[1];
+
+  return (
+    <Link
+      href={href}
+      className={`flex items-center gap-4 px-2.5 ${pathname !== hrefPath ? "text-muted-foreground hover:" : ""}text-foreground`}
+    >
+      {children}
+      {label}
+    </Link>
   );
 }
